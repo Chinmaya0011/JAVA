950b1a9c3f62b2ecb225e6aeca8037f4e5c30592
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-08-31 23:54:30 +0530

    Java Function

---
+++ function.java
@@ -0,0 +1,24 @@
+public class function {
+    public static void main(String[] args) {
+        // int ans=Sum(12,2322);
+        // System.out.println(ans);
+       String ans= greet("Chinmaya");
+       System.out.println(ans);
+    }
+//     static int Sum(int x,int y){
+// int a=x;
+// int b=y;
+// int sum=a+b;
+// return sum;
+
+//     }
+
+
+static String greet(String name){
+String greeting="Hello"+name;
+return greeting;
+}
+
+
+
+}
