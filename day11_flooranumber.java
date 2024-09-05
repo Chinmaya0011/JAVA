394a323d92eb7ea6aafc74a3becb7efb7bdfc131
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-09-05 11:58:38 +0530

    floor a number binary search array

--- day11_flooranumber.java
+++ day11_flooranumber.java
@@ -1,5 +1,26 @@
 public class day11_flooranumber {
     public static void main(String[] args) {
-        
+        int[] arr={1,3,5,8,9,17};
+        int target=17;
+        int ans=floor(arr,target);
+        System.out.println(ans);
+    }
+
+    static int floor(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target==arr[mid]){
+                return mid;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return end;
     }
 }
