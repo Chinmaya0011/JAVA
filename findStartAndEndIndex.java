af1389d76b1bc257e18644201ddbd844f08599d0
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-09-06 13:12:43 +0530

    first and start index

--- findStartAndEndIndex.java
+++ findStartAndEndIndex.java
@@ -2,7 +2,7 @@ import java.sql.Array;
 import java.util.Arrays;
 public class findStartAndEndIndex {
     public static void main(String[] args) {
-       int[] nums={5,7,7,8,8,10};
+       int[] nums={5,7,7,8,10};
        int target=8;
       System.out.println(Arrays.toString(searchRange(nums,target)));  
     }
